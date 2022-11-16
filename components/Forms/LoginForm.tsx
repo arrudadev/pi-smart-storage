9c7299def635ed81d4df3de305c49fd8f285d642
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2022-11-15 22:49:01 -0300

    refactor: add LoginForm component

---
+++ components/Forms/LoginForm.tsx
@@ -0,0 +1,56 @@
+export const LoginForm = () => {
+  return (
+    <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+      <div className="rounded-t mb-0 px-6 py-6">
+        <div className="text-center mb-3">
+          <h6 className="text-blueGray-500 text-sm font-bold">Login</h6>
+        </div>
+
+        <hr className="mt-6 border-b-1 border-blueGray-300" />
+      </div>
+
+      <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+        <form>
+          <div className="relative w-full mb-3">
+            <label
+              className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+              placeholder="Email"
+            />
+          </div>
+
+          <div className="relative w-full mb-3">
+            <label
+              className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+              htmlFor="password"
+            >
+              Senha
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+              placeholder="Senha"
+            />
+          </div>
+
+          <div className="text-center mt-6">
+            <button
+              className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+              type="button"
+            >
+              Entrar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,5 @@
 import { LoginFooter } from '../components/Footers/LoginFooter';
+import { LoginForm } from '../components/Forms/LoginForm';
 import { LoginNavbar } from '../components/Navbars/LoginNavbar';
 
 export default function Login() {
@@ -18,60 +19,7 @@ export default function Login() {
           <div className="container mx-auto px-4 h-full">
             <div className="flex content-center items-center justify-center h-full">
               <div className="w-full lg:w-4/12 px-4">
-                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-                  <div className="rounded-t mb-0 px-6 py-6">
-                    <div className="text-center mb-3">
-                      <h6 className="text-blueGray-500 text-sm font-bold">
-                        Sign in with
-                      </h6>
-                    </div>
-
-                    <hr className="mt-6 border-b-1 border-blueGray-300" />
-                  </div>
-
-                  <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                    <form>
-                      <div className="relative w-full mb-3">
-                        <label
-                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                          htmlFor="email"
-                        >
-                          Email
-                        </label>
-                        <input
-                          type="email"
-                          id="email"
-                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                          placeholder="Email"
-                        />
-                      </div>
-
-                      <div className="relative w-full mb-3">
-                        <label
-                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                          htmlFor="password"
-                        >
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          id="password"
-                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                          placeholder="Password"
-                        />
-                      </div>
-
-                      <div className="text-center mt-6">
-                        <button
-                          className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                          type="button"
-                        >
-                          Sign In
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                </div>
+                <LoginForm />
               </div>
             </div>
           </div>
