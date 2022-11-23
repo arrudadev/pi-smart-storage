a63dc68aba962bef69bf33b65d1adb0bc3863b1f
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2022-11-23 16:16:11 -0300

    fix: dashboard query

--- pages/api/dashboard.ts
+++ pages/api/dashboard.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -18,29 +18,28 @@ async function handleGetDashboard(
 
   const type: string | string[] = request.query.type ? request.query.type : '';
 
-  let movements = [];
+  let movements: any = [];
+
+  const initialDate = `${year}-${month}-01`;
+  const lastDate = `${year}-${month}-31`;
 
   if (productId) {
-    movements = await prisma.movements.findMany({
-      where: {
-        productId: Number(productId),
-        type: String(type),
-        date: {
-          lte: new Date(`${year}-${month}-31`),
-          gte: new Date(`${year}-${month}-01`),
-        },
-      },
-    });
+    movements = await prisma.$queryRaw(
+      Prisma.sql`
+        SELECT SUM(1) 'total', DATE_FORMAT(m.date, '%d') 'day' FROM Movements m
+        WHERE m.productId = ${productId}
+          AND m.type = ${type}
+          AND DATE_FORMAT(m.date, '%Y-%m-%d') BETWEEN ${initialDate} AND ${lastDate}
+        GROUP BY DATE(m.date)`,
+    );
   } else {
-    movements = await prisma.movements.findMany({
-      where: {
-        type: String(type),
-        date: {
-          lte: new Date(`${year}-${month}-31`),
-          gte: new Date(`${year}-${month}-01`),
-        },
-      },
-    });
+    movements = await prisma.$queryRaw(
+      Prisma.sql`
+        SELECT SUM(1) 'total', DATE_FORMAT(m.date, '%d') 'day' FROM Movements m
+        WHERE m.type = ${type}
+          AND DATE_FORMAT(m.date, '%Y-%m-%d') BETWEEN ${initialDate} AND ${lastDate}
+        GROUP BY DATE(m.date)`,
+    );
   }
 
   return response.status(200).json({
