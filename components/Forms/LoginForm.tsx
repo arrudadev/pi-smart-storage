8793c8b71b758f34a6d76d213ecae9bec8baa7cf
Alexandre Monteiro Arruda Junior <alexandre.monteiro.bec@gmail.com>
2022-11-26 01:18:39 +0000

    chore: add fake login test

--- components/Forms/LoginForm.tsx
+++ components/Forms/LoginForm.tsx
@@ -1,6 +1,24 @@
-import Link from 'next/link';
+import { ChangeEvent, useState } from 'react';
+
+import { useRouter } from 'next/router';
 
 export const LoginForm = () => {
+  const router = useRouter();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const fakeLoginEmail = 'admin@gmail.com';
+  const fakeLoginPassword = '12345678';
+
+  function handleFakeLogin() {
+    if (email === fakeLoginEmail && password === fakeLoginPassword) {
+      router.push('/admin/dashboard');
+    } else {
+      alert('Login inválido');
+    }
+  }
+
   return (
     <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
       <div className="rounded-t mb-0 px-6 py-6">
@@ -25,6 +43,10 @@ export const LoginForm = () => {
               id="email"
               className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
               placeholder="Email"
+              value={email}
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setEmail(event.target.value)
+              }
             />
           </div>
 
@@ -40,18 +62,21 @@ export const LoginForm = () => {
               id="password"
               className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
               placeholder="Senha"
+              value={password}
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setPassword(event.target.value)
+              }
             />
           </div>
 
           <div className="text-center mt-6">
-            <Link href="/admin/dashboard">
-              <button
-                className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                type="button"
-              >
-                Entrar
-              </button>
-            </Link>
+            <button
+              className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+              type="button"
+              onClick={handleFakeLogin}
+            >
+              Entrar
+            </button>
           </div>
         </form>
       </div>
